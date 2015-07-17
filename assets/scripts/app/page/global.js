c851c3b3db28326099c7dfb21fe23c3e02de8e0b
crespoapps <contact@crespoapps.com>
2015-07-17 16:47:31 -0700

    Async css and font load.

--- assets/scripts/app/page/global.js
+++ assets/scripts/app/page/global.js
@@ -1,5 +1,6 @@
 define([], function () {
-    function load () {
+    function load() {
+        $("head").append("<link href=\"//fonts.googleapis.com/css?family=Raleway:400,300,600\" rel=stylesheet type=text/css><link rel=stylesheet href=/assets/styles.min.css>");
     };
     return load;
 });
