3e20ab44d9d46bd9448c0e22f5a01b3232fa87ff
crespoapps <contact@crespoapps.com>
2015-12-16 17:06:26 -0800

    Xmass 2015 update
    Xmass 2015 update

--- assets/scripts/app/page/global.js
+++
@@ -1,6 +0,0 @@
-define([], function () {
-    function load() {
-        $("head").append("<link href=\"//fonts.googleapis.com/css?family=Raleway:400,300,600\" rel=stylesheet type=text/css><link rel=stylesheet href=/assets/styles.min.css>");
-    };
-    return load;
-});
